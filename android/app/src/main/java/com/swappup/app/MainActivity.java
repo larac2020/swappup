cfb00b58a2d35c6608e1c85eda167a021d631040
larac <user@unknown.home>
2026-08-07 13:59:23 +0100

    Add Capacitor setup

---
+++ android/app/src/main/java/com/swappup/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.swappup.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.swappup.app',
+  appName: 'swappup',
+  webDir: 'dist'
+};
+
+export default config;
